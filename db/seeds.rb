3327f31fff5132600c2da4cce7e16cf7cdc86302
Meghan Stang <mstang15@gmail.com>
2018-10-03 17:55:04 -0600

    adds user story 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,12 @@
+require './app/models/comedian.rb'
 
+tina = Comedian.create(name: "Tina Fey", age: 48, city: "Upper Darby, PA")
+ellen = Comedian.create(name: "Ellen Degeneres", age: 60, city: "Metairie, PA")
+whoopi = Comedian.create(name: "Whoopi Goldberg", age: 62, city: "New York City, NY")
+tig = Comedian.create(name: "Tig Notaro", age: 47, city: "Jackson, MS")
+amy = Comedian.create(name: "Amy Poehler", age: 47, city: "Newton, MA")
+chelsea = Comedian.create(name: "Chelsea Handler", age: 43, city: "Livingston, NJ")
+kristin = Comedian.create(name: "Kristin Wiig", age: 45, city: "Canandaigua, NY")
+ali = Comedian.create(name: "Ali Wong", age: 34, city: "San Francisco, CA")
+mindi = Comedian.create(name: "Mindi Kaling", age: 34, city: "Cambridge, MA")
+julia = Comedian.create(name: "Julia Louis-Dreyfus", age: 57, city: "New York City, NY")
